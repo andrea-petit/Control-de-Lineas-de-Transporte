4901336ef48cd6eab8edbc02b8584a82e17f76de
kroak0105 <164654578+AndresArias0105@users.noreply.github.com>
2025-11-03 16:43:17 -0400

    aja

---
+++ frontend/styles.py
@@ -0,0 +1,68 @@
+estilos_menu = '''
+    QWidget {
+    background: #0B2E73;
+    }
+
+    QFrame {
+    background: white;
+    border-radius: 5px;
+    }
+
+    QPushButton {
+    background: lightblue;
+    color: black;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: center;
+    }
+'''
+
+estilos_login = '''
+    QWidget {
+    background: #0B2E73;
+    }
+
+    QFrame {
+    background: white;
+    border-radius: 10px;
+    color: black;
+    font-size: 15px; 
+    font-weight: bold; 
+    text-align: center;
+    }
+
+    #frameTitle {
+    margin-top: 100px;}
+
+    QLineEdit {
+    background: LightBlue;
+    color: black;
+    padding: 15px;
+    font-size: 15px;
+    border-radius: 5px;
+    }
+
+    QLineEdit::placeholder {
+    color: gray;
+    }
+'''
+
+
+
+btnStyle = '''
+    QPushButton {
+        background: #012d51;
+        color: white;
+        padding: 15px;
+        border-radius: 10px;
+        border: none;
+        font-size: 13px;
+    }
+
+    QPushButton:hover {
+        background: #024a7a
+    }
+
+    QPushButton:pressed {
+        background: #011826;
+'''
